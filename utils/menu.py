1bb50b02d2589a0bcbe1fadccabc8a0c96f0a83a
Raian-Moretti <raian.moretti.eletro@gmail.com>
2023-04-29 01:13:42 -0300

    fix menu navigation

--- utils/menu.py
+++ utils/menu.py
@@ -3,7 +3,7 @@ from utils.plot import plot, importance
 def plot_options(clf,feature_cols, png_name, n_estimators=1):
     opt = -1
     while opt != 0:
-        print("\nO que deseja?\n1 - Gerar imagem da estrutura\n2 - Gráfico de importância de feature\n0 - Sair")
+        print("\nO que deseja?\n1 - Gerar imagem da estrutura\n2 - Gráfico de importância de feature\n0 - Voltar")
         try:
             opt = int(input())
         except ValueError:
@@ -14,6 +14,6 @@ def plot_options(clf,feature_cols, png_name, n_estimators=1):
         elif(opt == 2):
             importance(clf)
         elif(opt == 0):
-            print("\nFinalizado!")
+            print("\n### Menu inicial ###")
         else:
             print("\nOpção inválida")
